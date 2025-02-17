c613140686b064d175b600e49024b00c902b38b1
dwicks2113 <dwicks2113@outlook.com>
2025-02-17 13:20:20 -0500

    Clean up code

--- shinyproject/src/App.js
+++ shinyproject/src/App.js
@@ -5,36 +5,14 @@ import HomePage from './pages/Home.js'
 import AboutPage from './pages/About.js'
 import NotFoundPage from './pages/NotFound.js'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 function App () {
   return (
-    // <Router>
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/about' element={<AboutPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
-    // </Router>
-  )
+   )
 }
 
 export default App
