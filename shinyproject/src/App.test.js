63fd7163f25c3479d294861a268b99d91be2eb93
dwicks2113 <dwicks2113@outlook.com>
2025-02-17 11:28:25 -0500

    Added Home, About and NotFound pages

--- shinyproject/src/App.test.js
+++ shinyproject/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import { Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Home'
+import AboutPage from './pages/About'
+import NotFoundPage from './pages/NotFound'
+  
+
+function App () {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
