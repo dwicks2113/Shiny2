c67d5f895e731cfedb2cd602a764b7e9616b8bd2
dwicks2113 <dwicks2113@outlook.com>
2025-02-17 13:19:12 -0500

    Fixed router issues

--- shinyproject/src/App.js
+++ shinyproject/src/App.js
@@ -1,43 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import HomePage from './pages/Home.js'
-// import AboutPage from './pages/About.js'
-// import NotFoundPage from './pages/NotFound.js';
+//import logo from './logo.svg';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Home.js'
+import AboutPage from './pages/About.js'
+import NotFoundPage from './pages/NotFound.js'
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-// function App () {
+// function App() {
 //   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/' element={<HomePage />} />
-//         <Route path='/about' element={<AboutPage />} />
-//         <Route path='*' element={<NotFoundPage />} />
-//       </Routes>
-//     </Router>
-//   )
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
 // }
+function App () {
+  return (
+    // <Router>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+    // </Router>
+  )
+}
 
-
-
-export default App;
+export default App
