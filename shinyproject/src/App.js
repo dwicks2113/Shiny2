f132a2b3ceea451d04b222498dcc14b827963f7c
dwicks2113 <dwicks2113@outlook.com>
2025-02-17 13:07:59 -0500

    Ssync

--- shinyproject/src/App.js
+++ shinyproject/src/App.js
@@ -1,42 +1,42 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home.js'
-import AboutPage from './pages/About.js'
-import NotFoundPage from './pages/NotFound.js';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import HomePage from './pages/Home.js'
+// import AboutPage from './pages/About.js'
+// import NotFoundPage from './pages/NotFound.js';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-function App () {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+// function App () {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path='/' element={<HomePage />} />
+//         <Route path='/about' element={<AboutPage />} />
+//         <Route path='*' element={<NotFoundPage />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
 
 
 
