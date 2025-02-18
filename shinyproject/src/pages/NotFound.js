590eaba0f3acd88e589c53dcba7992f190333276
dwicks2113 <dwicks2113@outlook.com>
2025-02-18 10:47:51 -0500

    Added png files

--- shinyproject/src/pages/NotFound.js
+++ shinyproject/src/pages/NotFound.js
@@ -3,10 +3,9 @@ import React from 'react'
 function NotFoundPage () {
   return (
     <div>
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for cannot be found.</p>
+      <img src='../404.png' alt='Page Not Found' />
+      <p color='#FF6060'>Oops! The page you are looking for does not exist.</p>
     </div>
-  )
-}
-
+     )
+    }
 export default NotFoundPage
