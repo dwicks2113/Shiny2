9117d353ab4122b3c6baa0d16d90e541373ad6dd
dwicks2113 <dwicks2113@outlook.com>
2025-02-18 10:08:47 -0500

    Added Kasa Logo to project

--- shinyproject/src/components/Header.js
+++ shinyproject/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import logo from '../logo.svg';
+import logo from '../LOGO.png';
 
 function Header() {
     return (
