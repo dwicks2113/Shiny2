721e848b20ac08b0168675df2179471640de6183
dwicks2113 <dwicks2113@outlook.com>
2025-02-19 11:04:04 -0500

    Added background to Banner

--- shinyproject/src/components/Banner.js
+++ shinyproject/src/components/Banner.js
@@ -4,7 +4,9 @@ import './Banner.css';
 function Banner() {
     return (
         <section className='banner'>
+            <div className="overlay">
             <div className='banner-text'>At home, everywhere, and anywhere</div>
+            </div>
         </section>
     );
 }
