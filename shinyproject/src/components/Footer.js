d592626f6286c61f391e4dad2ff1ee4c033f5082
dwicks2113 <dwicks2113@outlook.com>
2025-02-20 09:54:24 -0500

    Add black Kasa logo to footer

--- shinyproject/src/components/Footer.js
+++ shinyproject/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Footer.css'
+import logo_footer from '../LOGO_footer.png'
 import footer_copyright from '../footer_copyright.png'
 
 function Footer () {
   return (
     <footer>
     <div className='footer-container'>
+      <img src={logo_footer} alt='logo' className='logo_footer' />
       <ul>
           <li>
             <Link to='/'>Return to Home page</Link>
