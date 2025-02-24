08a15b26d717537e61e0484c6738fae7eadc4b9e
dwicks2113 <dwicks2113@outlook.com>
2025-02-24 14:13:18 -0500

    Get image inside property box

--- shinyproject/src/components/Property.js
+++ shinyproject/src/components/Property.js
@@ -1,11 +1,24 @@
 import './Property.css';
 
-function Property({ title, img, id}) {
-    return ( 
-        <><div className='property'>{title}
-        <img src={img} alt={title} /></div>
-        </>
-     );
-}
+// 
+//function Property({ title, img, id}) {
+//     return ( 
+//         <><div className='property'>{title}
+//         <img src={img} alt={title} /></div>
+//         </>
+//      );
+// }
 
-export default Property;
+const Property = ({ title, img, id }) => {
+   const propertyStyle = {
+     backgroundImage: `url(${img})`
+   }
+
+return (
+   <div className='property' style={propertyStyle}>
+     <div className='property-title'>{title}</div>
+    </div>
+    );
+};
+
+   export default Property;
