51998981474f15dc7c2a3a3c9b5b0f770f301585
dwicks2113 <dwicks2113@outlook.com>
2025-02-27 13:19:03 -0500

    Add text shadow

--- shinyproject/src/pages/PropertyCard.js
+++ shinyproject/src/pages/PropertyCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const PropertyCard = ({ property, onClick }) => {
     return (
         <div onClick={onClick}>
-            <img src={property.cover[0]} alt={property.title} />
+            <img src={property.pictures[0]} alt={property.title} />
             <h2>{property.title}</h2>
             <p>{property.description}</p>
         </div>
