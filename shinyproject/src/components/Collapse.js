7fa335e840f33d45755b0c95924bcfb88954558e
dwicks2113 <dwicks2113@outlook.com>
2025-03-27 08:10:17 -0400

    Fixes to sass files

--- shinyproject/src/components/Collapse.js
+++ shinyproject/src/components/Collapse.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import "./Collapse.scss";
+import React, { useState } from 'react';
+import './Collapse.scss';
 
 function Collapse({ title, children }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,14 +8,15 @@ function Collapse({ title, children }) {
     };
 
     return (
-        <div className="collapse">
-        <button onClick={toggleCollapse} className="collapse-button" >
-        {title}
-        <span className={`arrow ${isOpen ? 'down' : 'right'}`}></span> 
-        </button>
-        {isOpen && <div className="collapse-content">{children}</div>}
+        <div className='collapse'>
+            <button onClick={toggleCollapse} className='collapse-button'>
+                {title}
+                <span className={`arrow ${isOpen ? 'down' : 'right'}`}></span>
+                {console.log('Arrow rendered with class:', isOpen ? 'down' : 'right')}
+            </button>
+            {isOpen && <div className='collapse-content'>{children}</div>}
         </div>
     );
-    }
+}
 
-    export default Collapse;
+export default Collapse;
