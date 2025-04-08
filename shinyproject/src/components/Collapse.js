f2ce21f2048520674febea904024ea15de0dfa2c
dwicks2113 <dwicks2113@outlook.com>
2025-04-08 15:38:18 -0400

    Worked on transitioning and mobile sizing

--- shinyproject/src/components/Collapse.js
+++ shinyproject/src/components/Collapse.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import './Collapse.scss';
+import React, { useState } from 'react'
+import './Collapse.scss'
 
-function Collapse({ title, children }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleCollapse = () => {
-        setIsOpen(!isOpen);
-    };
+function Collapse ({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen)
+    document.documentElement.style.setProperty(
+        '--page-height', isOpen ? 'auto' : '100vh'); // Set the height of the document to 100vh when the collapse is open
+  }
 
-    return (
-        <div className='collapse'>
-            <button onClick={toggleCollapse} className='collapse-button-about'>
-                {title}
-                <span className={`collapse-arrow ${isOpen ? 'down' : 'right'}`}></span>
-                {console.log('Arrow rendered with class:', isOpen ? 'down' : 'right')}
-            </button>
-            {isOpen && <div className='collapse-content'>{children}</div>}
-        </div>
-        );
-}        
-export default Collapse;
+  return (
+    <div className='collapse'>
+      <button onClick={toggleCollapse} className='collapse-button-about'>
+        {title}
+        <span className={`collapse-arrow ${isOpen ? 'down' : 'right'}`}></span>
+      </button>
+      {isOpen && <div className='collapse-content'>{children}</div>}
+    </div>
+  )
+}
+export default Collapse
